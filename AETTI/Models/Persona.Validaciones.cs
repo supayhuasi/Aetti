d125492aa8e59bb45685464a1fba33a7b1d0c9bb
supayhuasi <coriarodrigoc@gmail.com>
2015-06-15 20:40:24 -0300

    Solucion

---
+++ AETTI/Models/Persona.Validaciones.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AETTI.Models
+{
+    public partial class Persona
+    {
+    }
+
+    public class ProductoVentaValidation
+    {
+        public int Id { get; set; }
+
+
+        [DisplayName("C.U.I.T")]
+        [StringLength(100)]
+        public string CUIT { get; set; }
+        
+
+        
+        public string RazonSocial { get; set; }
+        public string CodigoPostal { get; set; }
+        public string Provincia { get; set; }
+        public string PartidoDepartamento { get; set; }
+        public string Localidad { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+    }
+}
